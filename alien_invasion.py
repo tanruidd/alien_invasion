5b9a4b222724700c2d632cb6ff5f6fe2dcab5653
trtan <1361957939@qq.com>
2019-11-09 10:48:11 +0800

    设置背景颜色

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,9 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((400, 400))
     pygame.display.set_caption("Alien Invasion")
+    
+    # set backgroud color
+    bg_color = (230, 230, 230)
 
     # start
     while True:
@@ -16,6 +19,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        screen.fill(bg_color)
         # view
         pygame.display.flip()
 
