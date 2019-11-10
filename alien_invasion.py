176bf733a823ff56a73264e60ff23e5881dc65cd
trtan <1361957939@qq.com>
2019-11-10 13:50:28 +0800

    删除消失子弹

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,13 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        # delete disappering bullets
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        #print(len(bullets))
+
         gf.update_screen(ai_settings, screen, ship, bullets)
 
         # view
