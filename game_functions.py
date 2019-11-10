57f291d42884725245925f373b4883fa43b1543c
trtan <1361957939@qq.com>
2019-11-10 12:00:21 +0800

    增加上下移动

--- game_functions.py
+++ game_functions.py
@@ -7,12 +7,21 @@ def check_keydown_events(event, ship):
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+
 
 def check_events(ship):
     """respond to keyboard and mouse events"""
--- ship.py
+++ ship.py
@@ -17,20 +17,28 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
 
         # save float
-        self.center = float(self.rect.centerx)
+        self.center_x = float(self.rect.centerx)
+        self.center_y = float(self.rect.bottom)
 
         #moving tag
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """ adjust the position of the ship"""
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
+            self.center_x += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > self.screen_rect.left:
-            self.center -= self.ai_settings.ship_speed_factor
+            self.center_x -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.center_y -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.center_y += self.ai_settings.ship_speed_factor
 
-        self.rect.centerx = self.center
+        self.rect.centerx = self.center_x
+        self.rect.bottom = self.center_y
 
     def blitme(self):
         """Draw"""
